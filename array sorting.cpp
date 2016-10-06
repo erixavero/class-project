d61b3283e0d3da019e791e28ffe3962ea07488fd
Eric Savero <MegaXaverix@gmail.com>
2016-10-06 19:29:51 +0700

    assignment

--- array sorting.cpp
+++ array sorting.cpp
@@ -2,32 +2,35 @@
 // sorted in descending order instead of ascending order.
 
 #include<iostream>
+#include<fstream>
+
 using namespace std;
 
  // This program shows the donations made to the United Cause
  // by the employees of CK Graphics, Inc. It displays
  // the donations in order from lowest to highest
  // and in the original order they were received.
- #include <iostream>
- using namespace std;
 
  // Function prototypes
- void inputArray(int [], int);
+ 
  void arrSelectSort(int *[], int);
  void showArray(const int [], int);
  void showArrPtr(int *[], int);
+ 
 
- int main()
- {
+ ofstream myfile;
+
+ int main(){
+ 	myfile.open("donation.txt");
  const int NUM_DONATIONS = 15; // Number of donations
 
  // An array containing the donation amounts.
- int donations[NUM_DONATIONS] = {  };
+ int donations[NUM_DONATIONS] = { 5, 100, 5, 25, 10,
+19, 5, 25, 5, 5, 100,
+20, 10, 15, 10};
 
  // An array of pointers to int.
  int *arrPtr[NUM_DONATIONS] = {};
- 
- inputArray(donations, NUM_DONATIONS);
 
  // Each element of arrPtr is a pointer to int. Make each
  // element point to an element in the donations array.
@@ -39,12 +42,15 @@ using namespace std;
 
  // Display the donations using the array of pointers. This
  // will display them in sorted order.
- cout << "The donations, sorted in descending order are: \n";
- showArrPtr(arrPtr, NUM_DONATIONS);
+ myfile<< "The donations, sorted in descending order are: \n";
+showArrPtr(arrPtr, NUM_DONATIONS);
+
 
  // Display the donations in their original order.
- cout << "The donations, in their original order are: \n";
+ myfile<< endl << "The donations, in their original order are: \n";
  showArray(donations, NUM_DONATIONS);
+ 
+ myfile.close();
  return 0;
  }
  //****************************************************************
@@ -86,9 +92,10 @@ using namespace std;
 
  void showArray(const int arr[], int size)
  {
+ string msgstr;
  for (int count = 0; count < size; count++)
- cout << arr[count] << " ";
- cout << endl;
+ myfile << arr[count] << " ";
+ myfile << endl;
  }
 
  //***************************************************************
@@ -100,15 +107,9 @@ using namespace std;
  void showArrPtr(int *arr[], int size)
  {
  for (int count = 0; count < size; count++)
- cout << *(arr[count]) << " ";
- cout << endl;
+ myfile << *(arr[count]) << " ";
+ myfile << endl;
  }
  
- void inputArray(int arr[], int size){
- 	cout<<"Give 15 donations" <<endl;
- 	for(int count=0; count<size; count++){
- 		cin>>arr[count];
-	 }
- }
  
  
