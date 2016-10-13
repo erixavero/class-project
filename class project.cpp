28af4b5b73c85e5e40c86b8f5937035ca3e4d4ad
Eric Savero <MegaXaverix@gmail.com>
2016-10-13 22:59:07 +0700

    class project math trial

--- class project.cpp
+++ class project.cpp
@@ -1,10 +1,18 @@
 #include<iostream>
+#include "math trial.cpp"
 using namespace std;
-
-
 
 int main(){
 	
+	int choice;
 	
+	cout<<"Welcome" <<endl
+		<<"1. Math trial	2. Decision game	3. Quiz" <<endl
+		<<"Take a pick: "; cin>>choice;
+	switch(choice){
+		case 1 : mathChoice();break;
+		
+		
+	}
 	
 return 0;}
--- math trial.cpp
+++ math trial.cpp
@@ -1,27 +1,46 @@
 #include<iostream>
 #include<cstdlib>
 #include<ctime>
+
 using namespace std;
-int main(){
+
+void mathPlus(int, int,  int, char);
+void mathMinus(int, int, int, char);
+
+void mathChoice(){
 	
 	unsigned seed= time(0);
 	srand(seed);
 	int randomNum=rand();
 	
 	int sign= randomNum%3;
-	int a= randomNum%900+126;
-	int b= randomNum%900+568;
-	int c= a+b;
+	int a= randomNum%900+560;
+	int b= randomNum%900+270;
+
 	int answer;
 	char more;
 	
+	int problem;
+	
 	do{
-	cout<<"MeTH GENERATOR" <<endl;
+	cout<<"MeTH GENERATOR" <<endl <<"1. (+)		2.(-)" <<endl
+		<<"Make a choice: "; cin>>problem;
+	switch(problem){
+		case 1 : mathPlus(a, b, answer, more);break;
+		case 2 : mathMinus(a, b, answer, more);break;
+	}
+} while(problem<0 && problem<3);
+}
+
+void mathPlus(int a, int b, int answer, char more){
+	
+	do{
 	if(a<1000){cout<<" ";} cout<< a <<endl;
 	if(b<1000){cout<<" ";}cout<< b <<endl;
-	cout<<"----- +"; 
+	cout<<"----- +" <<endl; 
 	cin>> answer;
 	
+	int c= a+b;
 	if (answer==c){
 		cout<<"OH JOY!" <<endl;
 	}
@@ -32,7 +51,32 @@ int main(){
 
 }while(more=='y');
 if(more=='n'){
-	cout<<"CHERRY BYE!";
+	cout<<"CHERRY BYE!" <<endl;
+	mathChoice();
+	}
 }
-return 0;
+
+void mathMinus(int a, int b, int answer, char more){
+	
+	do{
+	if(a<1000){cout<<" ";} cout<< a <<endl;
+	if(b<1000){cout<<" ";}cout<< b <<endl;
+	cout<<"----- -" <<endl; 
+	cin>> answer;
+	
+	int c= a-b;
+	if (answer==c){
+		cout<<"OH JOY!" <<endl;
+	}
+	else{cout<< "I AM DISAPPOINTED, THE ANSWER IS " << c <<endl;
+	}
+	do{cout<<"Want more?(y/n)"<<endl; cin>>more;}
+	while(more!='y' && more!='n');
+
+}while(more=='y');
+if(more=='n'){
+	cout<<"CHERRY BYE!" <<endl;
+	mathChoice();
+	}
+	
 }
