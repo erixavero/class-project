9d565a009f9497f215b015defeb3db6f7f52f5de
Eric Savero <MegaXaverix@gmail.com>
2016-10-21 00:35:49 +0700

    math done, need to count scores

--- math trial.cpp
+++ math trial.cpp
@@ -4,9 +4,10 @@
 
 using namespace std;
 
-void mathPlus(int, int,  int, char);
-void mathMinus(int, int, int, char);
-void mathMulti(int, int, int, char);
+void mathPlus(int, int,  int);
+void mathMinus(int, int, int);
+void mathMulti(int, int, int);
+void mathDivid(int, int, int);
 
 void mathChoice(){
 	
@@ -21,22 +22,22 @@ void mathChoice(){
 	int y= randomNum%617+70;
 
 	int answer;
-	char more;
-	
 	int problem;
 	
-	do{
-	cout<<"MeTH GENERATOR" <<endl <<"1.(+)		2.(-)		3.(X)" <<endl
-		<<"Make a choice: "; cin>>problem;
+//	do{
+	cout<<"MeTH GENERATOR" <<endl <<"1.(+)		2.(-)		3.(X)		4. (/) IGNORE DECIMALS" <<endl;
+//	cout<<"Your score: " <<score <<endl;
+	cout<<"Make a choice: "; cin>>problem;
 	switch(problem){
-		case 1 : mathPlus(a, b, answer, more);break;
-		case 2 : mathMinus(a, b, answer, more);break;
-		case 3 : mathMulti(x, y, answer, more);break;
+		case 1 : mathPlus(a, b, answer);break;
+		case 2 : mathMinus(a, b, answer);break;
+		case 3 : mathMulti(x, y, answer);break;
+		case 4 : mathDivid(x, y, answer);break;
 	}
-} while(problem<0 && problem<3);
+//} while(problem<0 && problem<5);
 }
 
-void mathPlus(int a, int b, int answer, char more){
+void mathPlus(int a, int b, int answer){
 	
 	int c= a+b;
 	if(a<1000){cout<<" ";} cout<< a <<endl;
@@ -47,13 +48,22 @@ void mathPlus(int a, int b, int answer, char more){
 	if (answer==c){
 		cout<<"OH JOY!" <<endl;
 	}
+	else if (answer==c && a%3==0){
+		cout<<"THE INTERNET SALUTES YOU!" <<endl;
+	}
+	else if (answer==c && a%7==0){
+		cout<<"YOU MADE IT LOOK EASY!" <<endl;
+	}
+	else if (answer!=c && a%7==0){
+		cout<< "I NEVER HAD FAITH IN YOU, THE ANSWER IS " << c <<endl;
+	}
 	else{cout<< "I AM DISAPPOINTED, THE ANSWER IS " << c <<endl;
 	}
 	mathChoice();
 }
 
 
-void mathMinus(int a, int b, int answer, char more){
+void mathMinus(int a, int b, int answer){
 	
 	int c= a-b;
 	if(a<1000){cout<<" ";} cout<< a <<endl;
@@ -64,24 +74,67 @@ void mathMinus(int a, int b, int answer, char more){
 	if (answer==c){
 		cout<<"EASY AIN'T IT?" <<endl;
 	}
+	else if (answer==c && a%3==0){
+		cout<<"EVERYTHING MAKES SENSE NOW!" <<endl;
+	}
+	else if (answer==c && a%7==0){
+		cout<<"I KNOW THIS ISN'T YOUR FIRST TIME!" <<endl;
+	}
+	else if (answer!=c && a%3==0){
+		cout<<"AND I THOUGHT MY JOKES WERE BAD! THE ANSWER IS " << c <<endl;
+	}
 	else{cout<< "YOUR MATH TEACHERS ARE ASHAMED! THE ANSWER IS " << c <<endl;
 	}
 	mathChoice();
 }
 
 
-void mathMulti(int x, int y, int answer, char more){
+void mathMulti(int x, int y, int answer){
 	
 	int c= x*y;
-	if(x<1000){cout<<" ";} cout<< x <<endl;
-	if(y<1000){cout<<" ";}cout<< y <<endl;
+	if(x<100){cout<<" ";} cout<< x <<endl;
+	if(y<100){cout<<" ";}cout<< y <<endl;
 	cout<<"----- x" <<endl; 
 	cin>> answer;
 	
 	if (answer==c){
 		cout<<"YOUR LIFE HAS MEANING AGAIN!" <<endl;
 	}
+	else if (answer==c && x%5==0){
+		cout<<"*SLOW CLAP*" <<endl;
+	}
+	else if (answer==c && c%3==0){
+		cout<<"YOU'RE THE MAN(OR WOMAN, IT DEPENDS) NOW!" <<endl;
+	}
+	else if (answer!=c && y%3==0){
+		cout<<"YOU WANT ROASTS WITH THAT? THE ANSWER IS " << c <<endl;
+	}
 	else{cout<< "MAYBE YOU PREFER CARD GAME? THE ANSWER IS " << c <<endl;
+	}
+	mathChoice();
+}
+
+void mathDivid(int x, int y, int answer){
+	int c= y/x;
+	if(y<100){cout<<" ";} cout<< y <<endl;
+	cout<<"-----" <<endl; 
+	if(x<100){cout<<" ";}cout<< x <<endl
+	<<"= ";
+	
+	cin>> answer;
+	if (answer==c){
+		cout<<"YOUR BRAIN HAS EVOLVED!" <<endl;
+	}
+	else if (answer==c && x%5==0){
+		cout<<"YOU GOT THE TOUCH!" <<endl;
+	}
+	else if (answer==c && c%9==0){
+		cout<<"ARE YOU STILL SINGLE?" <<endl;
+	}
+	else if (answer!=c && x%5==0){
+		cout<<"MY BUM IS ITCHY... THE ANSWER IS " << c <<endl;
+	}
+	else{cout<< "HELLO DARKNESS, MY OLD FRIEND. THE ANSWER IS " << c <<endl;
 	}
 	mathChoice();
 }
